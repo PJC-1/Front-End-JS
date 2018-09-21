3745c53db43e2f17d528a84746cc6c02315f115e
PJC-1 <jumpzine@gmail.com>
2018-09-20 20:22:08 -0700

    numLetters solution

--- numLetters.js
+++ numLetters.js
@@ -5,3 +5,17 @@
 // 3. the returned function should accept a number
 // 4. calling the returned function with a number should log out that many letters
 //
+
+var numLetters = function(letter) {
+    return new Function('integer',
+        "if (integer < 0 ) {return '';}\
+        var result = '';\
+        for (var i = 0; i < integer; i++) {\
+            result += '" + letter + "';\
+        }\
+        return result;"
+    );
+};
+
+console.log(numLetters('a')(3.8));
+// output => 'aaaa'
